c87126dc5278f9b00e6ce3861182f91b291af627
Nam Tran Trong <nam.tran-trong@ubisoft.com>
2023-09-05 22:27:59 +0700

    update

--- Assets/_Scripts/UI/GamePauseUI.cs
+++ Assets/_Scripts/UI/GamePauseUI.cs
@@ -1,42 +1,46 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using _Scripts;
-using _Scripts.UI;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GamePauseUI : MonoBehaviour
+namespace _Scripts.UI
 {
-    [SerializeField] private Button buttonContinue;
-    [SerializeField] private Button buttonMainMenu;
-    private void Awake()
+    public class GamePauseUI : MonoBehaviour
     {
-        Hide_ShowAlLChildObject(false);
-    }
+        [SerializeField] private Button buttonContinue;
+        [SerializeField] private Button buttonMainMenu;
+        private void Awake()
+        {
+            Hide_ShowAlLChildObject(false);
+        }
 
-    private void Start()
-    {
-        buttonContinue.onClick.AddListener(() =>
+        private void Start()
         {
-            GameManager.Instance.TogglePauseGame();
-        });
-        buttonMainMenu.onClick.AddListener(() =>
-        {
-            Loader.Load(Loader.Scene.MainMenuScene);
-        });
-        GameManager.Instance.OnTogglePauseGame += InstanceOnOnTogglePauseGame;
-    }
+            buttonContinue.onClick.AddListener(() =>
+            {
+                GameManager.Instance.TogglePauseGame();
+            });
+            buttonMainMenu.onClick.AddListener(() =>
+            {
+                Loader.Load(Loader.Scene.MainMenuScene);
+            });
+            GameManager.Instance.OnTogglePauseGame += InstanceOnOnTogglePauseGame;
+        }
 
-    private void InstanceOnOnTogglePauseGame(object sender, EventArgs e)
-    {
-        Hide_ShowAlLChildObject(GameManager.Instance.IsGamePause);
-    }
-    private void Hide_ShowAlLChildObject(bool active)
-    {
-        foreach (Transform child in transform)
+        private void OnDestroy()
         {
-            child.gameObject.SetActive(active);
+            GameManager.Instance.OnTogglePauseGame -= InstanceOnOnTogglePauseGame;
+        }
+
+        private void InstanceOnOnTogglePauseGame(object sender, EventArgs e)
+        {
+            Hide_ShowAlLChildObject(GameManager.Instance.IsGamePause);
+        }
+        private void Hide_ShowAlLChildObject(bool active)
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(active);
+            }
         }
     }
 }
